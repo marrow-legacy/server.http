7c764705a98a749c7b249d5cafdc18533a8cefea
Alice Bevan-McGregor <alice.mcgregor@me.com>
2010-05-13 12:12:26 -0700

    Initial template structure.

---
+++ Makefile
@@ -0,0 +1,39 @@
+.PHONY: clean update install develop devel docs tests test release
+
+test: tests
+
+clean:
+	rm -rvf *.egg-info
+	rm -rvf build
+	rm -rvf dist
+	find . -iname \*.pyc -or -iname \*.pyo -exec rm -vf {} \;
+	rm -rvf docs/build
+
+update:
+	git pull origin master
+
+install:
+	python setup.py install
+
+develop:
+	cp -f setup.cfg-devel setup.cfg
+	python setup.py develop
+
+devel: develop
+
+docs:
+	@mkdir -p docs/build/html
+	sphinx-build -b html -d docs/build/doctrees docs/source build/html
+
+.testing-deps:
+	pip install -q nose coverage
+	pip install git+git://github.com/exogen/nose-achievements.git
+	@touch .testing-deps
+
+tests: .testing-deps
+	python setup.py nosetests
+
+release: tests docs
+	cp -f setup.cfg-release setup.cfg
+	python setup.py register sdist bdist_egg upload upload_docs
+	cp -f setup.cfg-devel setup.cfg
---
+++ setup.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import sys, os
+
+try:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+
+except ImportError:
+    pass
+
+from setuptools import setup, find_packages
+
+
+if sys.version_info <= (2, 5):
+    raise SystemExit("Python 2.5 or later is required.")
+
+if sys.version_info >= (3,0):
+    def execfile(filename, globals_=None, locals_=None):
+        if globals_ is None:
+            globals_ = globals()
+        
+        if locals_ is None:
+            locals_ = globals_
+        
+        exec(compile(open(filename).read(), filename, 'exec'), globals_, locals_)
+
+else:
+    from __builtin__ import execfile
+
+execfile(os.path.join("pulp", "util", "release.py"), globals(), locals())
+
+
+
+setup(
+        name = name,
+        version = version,
+        
+        description = summary,
+        long_description = description,
+        author = author,
+        author_email = email,
+        url = url,
+        download_url = download_url,
+        license = license,
+        keywords = '',
+        
+        install_requires = [],
+        
+        test_suite = 'nose.collector',
+        tests_require = ['nose', 'coverage', 'nose-achievements'],
+        
+        classifiers = [
+                "Development Status :: 4 - Beta",
+                "Environment :: Console",
+                "Intended Audience :: Developers",
+                "License :: OSI Approved :: MIT License",
+                "Operating System :: OS Independent",
+                "Programming Language :: Python",
+                "Topic :: Software Development :: Libraries :: Python Modules"
+            ],
+        
+        packages = find_packages(exclude=['tests', 'tests.*', 'docs']),
+        include_package_data = True,
+        package_data = {
+                '': ['Makefile', 'README.textile', 'LICENSE', 'distribute_setup.py'],
+                'docs': ['source/*']
+            },
+        zip_safe = True,
+        
+        namespace_packages = ['pulp'],
+    )
