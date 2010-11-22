233912fd8c340dc7a7a5d260ef61d2842102d0c2
Alice Bevan-McGregor <alice.mcgregor@me.com>
2010-11-22 06:45:23 -0800

    Removed example from module.

--- marrow/server/http/__init__.py
+++ marrow/server/http/__init__.py
@@ -30,15 +30,3 @@ class HTTPServer(Server):
         fcntl.fcntl(sock.fileno(), fcntl.F_SETFD, flags)
         
         return sock
-
-
-if __name__ == '__main__':
-    import logging
-    
-    logging.basicConfig(level=logging.DEBUG)
-    
-    def hello(request):
-        # request['wsgi.errors'].write("Oh noes!") # Example error, output through the logging module.  \o/
-        return b'200 OK', [(b'Content-Type', b'text/plain'), (b'Content-Length', b'12')], [b'Hello world!']
-    
-    HTTPServer(None, 8080, fork=1, application=hello).start()
