22f6a766ee644ad5affb0c5e22fe46799bd277bf
Alice Bevan-McGregor <alice.mcgregor@me.com>
2010-12-01 09:38:04 -0800

    Switched to using the HTTP unit test helper.

--- tests/test_http10.py
+++ tests/test_http10.py
@@ -6,8 +6,7 @@ import socket
 
 from pprint import pformat
 
-from marrow.server.testing import ServerTestCase
-from marrow.server.http.protocol import HTTPProtocol
+from marrow.server.http.testing import HTTPTestCase, CRLF, EOH
 
 from marrow.util.compat import unicode
 
@@ -17,6 +16,11 @@ log = __import__('logging').getLogger(__name__)
 
 
 def echo(request):
+    del request['SERVER_NAME']
+    del request['SERVER_PORT']
+    del request['wsgi.errors']
+    del request['wsgi.input']
+    
     result = unicode(pformat(request)).encode('utf8')
     return b'200 OK', [
             (b'Content-Type', b'text/plain; charset=utf8'),
@@ -24,57 +28,53 @@ def echo(request):
         ], [result]
 
 
-class TestHTTP10Protocol(ServerTestCase):
-    protocol = HTTPProtocol
+class TestHTTP10Protocol(HTTPTestCase):
     arguments = dict(application=echo)
     
     def test_headers(self):
-        self.client.write(b"GET / HTTP/1.0\r\n\r\n")
-        self.client.read_until(b"\r\n\r\n", self.stop)
-        
-        headers = self.wait()
-        
-        self.assertEquals(headers, b'HTTP/1.0 200 OK\r\nContent-Type: text/plain; charset=utf8\r\nContent-Length: 605\r\n\r\n')
+        response = self.request(protocol=b"HTTP/1.0")
+        self.assertEquals(response.protocol, b"HTTP/1.0")
+        self.assertEquals(response.code, b"200")
+        self.assertEquals(response.status, b"OK")
+        self.assertEquals(response[b'content-type'], b"text/plain; charset=utf8")
+        self.assertEquals(response[b'content-length'], b"438")
     
     def test_request(self):
-        self.client.write(b"GET / HTTP/1.0\r\n\r\n")
-        self.client.read_until(b"\r\n\r\n", self.stop)
-        headers = self.wait()
+        response = self.request(protocol=b"HTTP/1.0")
+        request = eval(response.body)
         
-        self.client.read_bytes(605, self.stop)
-        body = self.wait()
+        expect = {
+                'CONTENT_LENGTH': None,
+                'CONTENT_TYPE': None,
+                'FRAGMENT': '',
+                'HTTP_HOST': 'localhost',
+                'PARAMETERS': '',
+                'PATH_INFO': '/',
+                'QUERY_STRING': '',
+                'REMOTE_ADDR': '127.0.0.1',
+                'REQUEST_METHOD': 'GET',
+                'SCRIPT_NAME': '',
+                'SERVER_ADDR': '127.0.0.1',
+                'SERVER_PROTOCOL': 'HTTP/1.0',
+                'wsgi.multiprocess': False,
+                'wsgi.multithread': False,
+                'wsgi.run_once': False,
+                'wsgi.url_scheme': 'http',
+                'wsgi.version': (2, 0)
+            }
         
-        # EVAL the body to produce a dict, remove SERVER_NAME, SERVER_PORT, wsgi.errors, wsgi.input, and compare.
+        self.assertEquals(request, expect)
     
     def test_single(self):
-        self.client.write(b"GET / HTTP/1.0\r\n\r\n")
-        self.client.read_until(b"\r\n\r\n", self.stop)
-        headers = self.wait()
-        
-        self.client.read_bytes(605, self.stop)
-        body = self.wait()
+        self.request(protocol=b"HTTP/1.0")
         
         def try_again():
-            self.client.write(b"GET / HTTP/1.0\r\n\r\n")
-            self.client.read_until(b"\r\n\r\n", self.stop)
-            headers = self.wait()
+            self.request(protocol=b"HTTP/1.0")
         
         self.assertRaises(socket.error, try_again)
     
     def test_keepalive(self):
-        self.client.write(b"GET / HTTP/1.0\r\nConnection: keep-alive\r\n\r\n")
-        self.client.read_until(b"\r\n\r\n", self.stop)
-        headers1 = self.wait()
+        one = self.request(protocol=b"HTTP/1.0", headers=[(b'Connection', b'keep-alive')])
+        two = self.request(protocol=b"HTTP/1.0", headers=[(b'Connection', b'keep-alive')])
         
-        self.client.read_bytes(640, self.stop)
-        body1 = self.wait()
-        
-        self.client.write(b"GET / HTTP/1.0\r\nConnection: keep-alive\r\n\r\n")
-        self.client.read_until(b"\r\n\r\n", self.stop)
-        headers2 = self.wait()
-        
-        self.client.read_bytes(640, self.stop)
-        body2 = self.wait()
-        
-        self.assertEquals(headers1, headers2)
-
+        self.assertEquals(one, two)
