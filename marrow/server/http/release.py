d3505b838b0ef1a5a4149385e1485798fe3e4de8
Alice Bevan-McGregor <alice@gothcandy.com>
2011-08-19 19:20:58 -0400

    Unification of release information, setup script, and setup configuration.

--- marrow/server/http/release.py
+++ marrow/server/http/release.py
@@ -1,5 +1,21 @@
 # encoding: utf-8
 
+"""Release information about Marrow HTTP Server."""
+
+from collections import namedtuple
+
+
+__all__ = ['version_info', 'version']
+
+
+version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(0, 9, 0, 'final', 0)
+
+version = ".".join([str(i) for i in version_info[:3]]) + ((version_info.releaselevel[0] + str(version_info.serial)) if version_info.releaselevel != 'final' else '')
+
+
+
+# encoding: utf-8
+
 """Release information about Marrow."""
 
 
--- setup.py
+++ setup.py
@@ -1,67 +1,75 @@
 #!/usr/bin/env python
-import sys
-import os
+# encoding: utf-8
 
-from distribute_setup import use_setuptools
-use_setuptools()
+import os
+import sys
 
 from setuptools import setup, find_packages
 
 
-if sys.version_info <= (2, 6):
+if sys.version_info < (2, 6):
     raise SystemExit("Python 2.6 or later is required.")
 
 exec(open(os.path.join("marrow", "server", "http", "release.py")))
 
+
+
 setup(
-    name=name,
-    version=version,
-    description=summary,
-    long_description=description,
-    author=author,
-    author_email=email,
-    url=url,
-    download_url=download_url,
-    license=license,
-    keywords='',
+        name="marrow.server.http",
+        version=version,
+        
+        description="A fast, multi-process, multi-threaded, asynchronous HTTP/1.1-compliant WSGI 2 server.",
+        long_description = """\
+For full documentation, see the README.textile file present in the package,
+or view it online on the GitHub project page:
 
-    install_requires=[
-        'marrow.util < 2.0',
-        'marrow.server < 2.0'
-    ],
-    extras_require={'script': ['marrow.script >= 1.1']},
-
-    test_suite='nose.collector',
-    tests_require=['nose', 'coverage'],
-
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.1",
-        "Programming Language :: Python :: 3.2",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
-        "Topic :: Internet :: WWW/HTTP :: WSGI",
-    ],
-
-    packages=find_packages(exclude=['tests', 'tests.*', 'docs']),
-    include_package_data=True,
-    package_data={
-        '': ['Makefile', 'README.textile', 'LICENSE', 'distribute_setup.py'],
-        'docs': ['source/*']
-    },
-    zip_safe=True,
-    namespace_packages=['marrow', 'marrow.server'],
-
-    entry_points={
-        'console_scripts': [ 'marrow.httpd = marrow.server.http.command:main [script]' ],
-        'marrow.server': ['http = marrow.server.http:HTTPServer']
-    }
+https://github.com/marrow/marrow.server.http""",
+        
+        author = "Alice Bevan-McGregor",
+        author_email = "alice+marrow@gothcandy.com",
+        url = "https://github.com/marrow/marrow.server.http",
+        license = "MIT",
+        
+        install_requires=[
+            'marrow.util < 2.0',
+            'marrow.server < 2.0'
+        ],
+        
+        extras_require = dict(
+            script = [
+                    'marrow.script >= 1.1'
+                ]
+        ),
+        
+        test_suite='nose.collector',
+        tests_require=['nose', 'coverage'],
+        
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Environment :: Console",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2.6",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.1",
+            "Programming Language :: Python :: 3.2",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
+            "Topic :: Internet :: WWW/HTTP :: WSGI",
+        ],
+        
+        packages = find_packages(exclude=['examples', 'tests']),
+        zip_safe = True,
+        include_package_data = True,
+        package_data = {'': ['README.textile', 'LICENSE']},
+        
+        namespace_packages = ['marrow', 'marrow.server'],
+        
+        entry_points = {
+            'console_scripts': [ 'marrow.httpd = marrow.server.http.command:main [script]' ],
+            'marrow.server': ['http = marrow.server.http:HTTPServer']
+        }
 )
